370a622fc755998630023e251bdfa4b068b6222e
doedre <lmlypp@gmail.com>
2021-07-31 18:53:55 +0500

    Just added firs lines of code

---
+++ src/main.c
@@ -0,0 +1,53 @@
+/*	main.c
+ *
+ * The main file, where the 'main' function located. At first it controls
+ * flags that were added to the program call, then it decides what to do.
+ *
+ * ----------------------------------------------------------------------
+ *
+ * This file is part of the radexi-cmd program - C language implementation of 
+ * RADEX software package. It is used to calculate molecular exitation and 
+ * radiative transfer in a homogenuous medium. The purpose of this full 
+ * refactoring is to speed up the process of calculation and implement some new
+ * features like databases. It will make the program more stable and usable, 
+ * which is crucial for newcomers.
+ *
+ * Documentation for radexi-cmd program is posted at 
+ * https://github.com/doedre/radexi-cmd
+ *
+ * ----------------------------------------------------------------------
+ *
+ *  From the RADEX software package:
+ * 	Documentation for the RADEX program is posted at
+ *	https://personal.sron.nl/~vdtak/radex/index.shtml 
+ *
+ *	Although this program has been thoroughly tested, the authors do not 
+ *	claim that it is free of errors and gives correct results in all 
+ *	situations.
+ *	
+ *	Publications using this program should make a reference to the paper:
+ *	A&A 468, 627 (2007).
+ *
+ * ---------------------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main( int argc, char * argv[] )
+{
+	int opt;
+	while ( (opt = getopt( argc, argv, "h" )) != -1 ) {
+		switch ( opt ) {
+			case 'h':
+				break;
+			default:
+				printf( "Usage: %s flags...\n", 
+						argv[0] );
+				exit( EXIT_FAILURE );
+		}
+	}
+
+	exit( EXIT_SUCCESS );
+}
