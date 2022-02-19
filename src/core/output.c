9038f7170a5c6ffcbc0068b1574c97f6a6ede56a
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2022-02-19 16:11:14 +0500

    Fix output not initialized in repo

---
+++ src/core/output.c
@@ -0,0 +1,263 @@
+/**
+ * @file output.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+
+#include "rxi_common.h"
+#include "core/output.h"
+#include "utils/cli_tools.h"
+#include "utils/debug.h"
+
+int
+rxi_check_path (const char *path)
+{
+  struct stat sb;
+  stat(path, &sb);
+
+  if (S_ISREG (sb.st_mode))
+      return 1;
+  else if (S_ISDIR (sb.st_mode))
+      return 2;
+
+  FILE *check;
+  check = fopen (path, "w");
+  if (check)
+    {
+      fclose (check);
+      return 0;
+    }
+  else 
+    {
+      return -1;
+    }
+}
+
+void
+rxi_out_print (const struct rxi_calc_data *data,
+               const struct rxi_calc_results *results)
+{
+  DEBUG ("Print resuts to command line");
+  char *geometry = geomtoname (data->input.geom);
+  
+  printf ("* Radexi version             : " RXI_VERSION "\n");
+  printf ("* Geometry                   : %s\n", geometry);
+  free (geometry);
+  printf ("* Molecule                   : %s\n", data->input.name);
+  printf ("* Kinetic temperature    [K] : %.3f\n", data->input.temp_kin);
+  printf ("* Background temperature [K] : %.3f\n", data->input.temp_bg);
+  printf ("* Column density      [cm-2] : %.3e\n", data->input.col_dens);
+  printf ("* Line width          [km/s] : %.3f\n", data->input.line_width);
+  for (int8_t i = 0; i < data->input.n_coll_partners; i++)
+    {
+      char *cp_name = numtoname (data->input.coll_part[i]);
+      printf ("* Density of %9s[cm-3] : %.3e\n", cp_name,
+              data->input.coll_part_dens[i]);
+      free (cp_name);
+    }
+
+  printf ("*    LINE       E_UP          FREQ         WAVEL        T_EX       \
+  TAU         T_R         POP         POP        FLUX       FLUX\n");
+  printf ("*                [K]         [GHz]          [nm]         [K]       \
+              [K]          UP         LOW    [K km/s]   [erg cm-2 s-1]\n");
+
+  char line_format[120];
+  strcpy (line_format, "%3d -> %3d  %8.1f  %12.4f  %12.4f  %10.3f  %10.3e \
+ %10.3e  %10.3e  %10.3e  %10.3e  %10.3e  %2d\n");
+
+  for (unsigned int i = 0; i < data->numof_radtr; ++i)
+    {
+      const int u = results[i].up - 1;
+      const int l = results[i].low - 1;
+      const float freq = results[i].spfreq;
+
+      if ((freq < data->input.sfreq) || (freq > data->input.efreq))
+        continue;
+
+      int blend_count = 0;
+      float line_width_freq = data->input.line_width * freq * 1e5 / RXI_SOL;
+      for (size_t j = 0; j < data->numof_radtr; ++j)
+        {
+          if (i == j)
+            continue;
+
+          float overlap = fabs (freq - results[j].spfreq);
+          if (overlap <= line_width_freq)
+            ++blend_count;
+
+          if ((blend_count > 0) && (overlap > line_width_freq))
+            break;
+        }
+
+      const double xt = gsl_pow_3 (results[i].xnu);
+      printf (line_format, u, l, freq * 1e9 * RXI_HP / RXI_KB, freq,
+          RXI_SOL / freq / 1e5, results[i].excit_temp, results[i].tau,
+          results[i].antenna_temp, gsl_vector_get (data->pop, u),
+          gsl_vector_get (data->pop, l),
+          1.0645 * data->input.line_width * results[i].antenna_temp,
+          1.0645 * 8 * M_PI * RXI_KB * data->input.line_width
+            * results[i].antenna_temp * xt,
+          blend_count);
+    }
+}
+
+void
+rxi_out_result_sort (struct rxi_calc_results *results, size_t results_size)
+{
+  DEBUG ("Sort results by spectral frequencies");
+  for (size_t i = 0; i < results_size; ++i)
+    {
+      size_t e = i;
+      for (size_t j = i; j < results_size; ++j)
+        {
+          if (results[j].spfreq < results[e].spfreq)
+            e = j;
+        }
+      struct rxi_calc_results element = results[e];
+      results[e] = results[i];
+      results[i] = element;
+    }
+}
+
+RXI_STAT
+rxi_out_result (const struct rxi_calc_data *data,
+                const struct rxi_options *opts)
+{
+  struct rxi_calc_results output[data->numof_radtr];
+  for (size_t i = 0; i < data->numof_radtr; ++i)
+    {
+      output[i].up = data->up[i];
+      output[i].low = data->low[i];
+      output[i].spfreq = gsl_matrix_get (data->freq, data->up[i] - 1,
+                                         data->low[i] - 1);
+      output[i].xnu = gsl_vector_get (data->term, output[i].up - 1)
+                      - gsl_vector_get (data->term, output[i].low - 1);
+      output[i].tau = gsl_matrix_get (data->tau, data->up[i] - 1,
+                                      data->low[i] - 1);
+      output[i].excit_temp =  gsl_matrix_get (data->excit_temp,
+          data->up[i] - 1, data->low[i] - 1);
+      output[i].antenna_temp =  gsl_matrix_get (data->antenna_temp,
+          data->up[i] - 1, data->low[i] - 1);
+    }
+
+  rxi_out_result_sort (output, data->numof_radtr);
+  
+  if (opts->cmd_output)
+    rxi_out_print (data, output);
+
+  if (opts->no_result_file)
+    return RXI_OK;
+
+  char *path;
+  path = malloc (RXI_PATH_MAX);
+  CHECK (path);
+  if (!path)
+    return RXI_ERR_ALLOC;
+  strcpy (path, opts->result_path);
+
+  struct stat sb;
+  stat (opts->result_path, &sb);
+  if (S_ISDIR (sb.st_mode))
+    {
+      strcat (path, "/");      
+      strcat (path, "rxi_out.txt");
+    }
+
+  stat (path, &sb);
+  if (S_ISREG (sb.st_mode) && !opts->force_fs)
+    {
+      printf ("  ## Specified file `%s` already exists. Rewrite it?\n", path);
+      if (!rxi_readline_accept ())
+        {
+          free (path);
+          return RXI_OK;
+        }
+    }
+
+  DEBUG ("Opening `%s` file for output result", path);
+  FILE *result_file;
+  result_file = fopen (path, "w");
+  if (!result_file)
+    {
+      free (path);
+      return RXI_ERR_FILE;
+    }
+
+  char *geometry = geomtoname (data->input.geom);
+  
+  fprintf (result_file, "* Radexi version             : " RXI_VERSION "\n");
+  fprintf (result_file, "* Geometry                   : %s\n", geometry);
+  free (geometry);
+  fprintf (result_file, "* Molecule                   : %s\n",
+           data->input.name);
+  fprintf (result_file, "* Kinetic temperature    [K] : %.3f\n",
+           data->input.temp_kin);
+  fprintf (result_file, "* Background temperature [K] : %.3f\n",
+           data->input.temp_bg);
+  fprintf (result_file, "* Column density      [cm-2] : %.3e\n",
+           data->input.col_dens);
+  fprintf (result_file, "* Line width          [km/s] : %.3f\n",
+           data->input.line_width);
+  for (int8_t i = 0; i < data->input.n_coll_partners; i++)
+    {
+      char *cp_name = numtoname (data->input.coll_part[i]);
+      fprintf (result_file, "* Density of %9s[cm-3] : %.3e\n", cp_name,
+               data->input.coll_part_dens[i]);
+      free (cp_name);
+    }
+
+  fprintf (result_file,     "*    LINE       E_UP          FREQ         WAVEL \
+       T_EX         TAU         T_R         POP         POP        FLUX       \
+FLUX\n");
+  fprintf (result_file,     "*                [K]         [GHz]          [nm] \
+       [K]                      [K]          UP         LOW    [K km/s]   \
+[erg cm-2 s-1]\n");
+
+  char line_format[120];
+  strcpy (line_format, "%3d -> %3d  %8.1f  %12.4f  %12.4f  %10.3f  %10.3e \
+ %10.3e  %10.3e  %10.3e  %10.3e  %10.3e  %2d\n");
+
+  for (unsigned int i = 0; i < data->numof_radtr; ++i)
+    {
+      const int u = output[i].up - 1;
+      const int l = output[i].low - 1;
+      const float freq = output[i].spfreq;
+
+      if ((freq < data->input.sfreq) || (freq > data->input.efreq))
+        continue;
+
+      int blend_count = 0;
+      float line_width_freq = data->input.line_width * freq * 1e5 / RXI_SOL;
+      for (size_t j = 0; j < data->numof_radtr; ++j)
+        {
+          if (i == j)
+            continue;
+
+          float overlap = fabs (freq - output[j].spfreq);
+          if (overlap <= line_width_freq)
+            ++blend_count;
+
+          if ((blend_count > 0) && (overlap > line_width_freq))
+            break;
+        }
+
+      const double xt = gsl_pow_3 (output[i].xnu);
+      fprintf (result_file, line_format, u, l, freq * 1e9 * RXI_HP / RXI_KB,
+          freq, RXI_SOL / freq / 1e5, output[i].excit_temp, output[i].tau,
+          output[i].antenna_temp, gsl_vector_get (data->pop, u),
+          gsl_vector_get (data->pop, l),
+          1.0645 * data->input.line_width * output[i].antenna_temp,
+          1.0645 * 8 * M_PI * RXI_KB * data->input.line_width
+            * output[i].antenna_temp * xt,
+          blend_count);
+    }
+  fclose (result_file);
+  free (path);
+  return RXI_OK;
+}
